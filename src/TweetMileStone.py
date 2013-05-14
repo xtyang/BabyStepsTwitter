0cd0762a3d771b9174dd94466e5c059927803864
Chun-Ku Lin <chunku@uw.edu>
2013-05-14 10:58:48 -0700

    Swap tweetmilestone
    Make TweetMileStone the updated one.

--- src/TweetMileStone.py
+++ src/TweetMileStone.py
@@ -1,106 +1,105 @@
 '''
-Created on Feb 28, 2013
+Created on Apr 22, 2013
 
 @author: Chun-Ku
 '''
 
+#=========================import library=======================================
 import twitter
 import threading
-import sqlite3
 import datetime
+import MySQLdb
+
+def calculateCurrentAge(bornYear, bornMonth):
+    now = datetime.datetime.now()
+    currentAge = (now.year - bornYear)*12 + now.month - bornMonth
+    return currentAge
+
+def selectMilestoneQuery(currentAge, most_recent_milestone):
+    currentAge = 6 #for testing
+    query = "SELECT questionID, text FROM questionnairecontent WHERE month = "+str(currentAge) +" AND questionID > "+ str(most_recent_milestone) +" ORDER BY questionID ASC;"
+    return query
 
 def postTweet(database, account):
-    print 'postTweet'
-    api = twitter.Api(consumer_key=account[2], consumer_secret=account[3], access_token_key=account[4], access_token_secret=account[5])
-    cur = database.cursor()
-    query_for_most_recent_updates = 'SELECT milestone_id, month FROM twitter_updates WHERE twitter_id = '+ str(account[0])
-    cur.execute(query_for_most_recent_updates) 
-    row = cur.fetchone()
-    print query_for_most_recent_updates
-    print row
-    updateText = None
-    milestone_id = None
-    now = datetime.datetime.now()
-    print now
-    month = (now.year - account[6])*12 + now.month - account[7] 
+    print "postTweet for account "+ account[1]
+    '''
+    twitteraccount table
+    account[0]: id
+    account[1]: account_name
+    account[2]: consumter_key
+    account[3]: consumer_secret
+    account[4]: access_token_key
+    account[5]: access_token_secret
+    account[6]: bornYear
+    account[7]: bornMonth
+    account[8]: most_recent_milestone
     
-    if row == None:#the first milestone to post
-        print 'first milestone to post'
-        query_for_corresponding_updates = 'SELECT milestone_id FROM month_milestones WHERE month = ' + str(month)
-        print query_for_corresponding_updates
-        cur.execute(query_for_corresponding_updates)
-        row = cur.fetchone() #fetch the first milestone to update
-        if row != None:
-            print 'fisrt milestone text to update'
-            milestone_id = row[0]
-            query_milestone_text = 'SELECT text FROM milestones WHERE id = ' + str(milestone_id)
-            cur.execute(query_milestone_text)
-            row = cur.fetchone()
-            updateText = row[0]
+    TODO: use macro to for table fields make code readable and easy to maintain
+    ''' 
+    currentAge = calculateCurrentAge(account[6], account[7])
+    print "current age = "+str(currentAge)
+    query = selectMilestoneQuery(currentAge, account[8])
+    print query
+    cursor = database.cursor()
+    cursor.execute(query)
+    row = cursor.fetchone()
+    
+    if row == None:
+        #TODO: select activiy or tip to tweet
+        print "select activity or tip to tweet"
+        cursor.close()
+        return None
     else:
-        if row[1] == month:
-            query_for_corresponding_updates = 'SELECT milestone_id FROM month_milestones WHERE month = ' + str(month)+' AND milestone_id > '+str(row[0])
-            print query_for_corresponding_updates
-            cur.execute(query_for_corresponding_updates)
-            #row[0] is the most recent milestone_id
-            row = cur.fetchone()
-            print row
-            if row != None:
-                milestone_id = row[0]
-                query_milestone_text = 'SELECT text FROM milestones WHERE id = ' + str(milestone_id)
-                cur.execute(query_milestone_text)
-                row = cur.fetchone()
-                updateText = row[0]
-        else:
-            query_for_corresponding_updates = 'SELECT milestone_id FROM month_milestones WHERE month = ' + str(month)
-            print query_for_corresponding_updates
-            cur.execute(query_for_corresponding_updates)
-            row = cur.fetchone() #fetch the first milestone to update
-            if row != None:
-                print 'fisrt milestone text to update'
-                milestone_id = row[0]
-                query_milestone_text = 'SELECT text FROM milestones WHERE id = ' + str(milestone_id)
-                cur.execute(query_milestone_text)
-                row = cur.fetchone()
-                updateText = row[0]
-            
-         
-    if updateText != None:
-        print str(updateText)
-        api.PostUpdate(str(updateText)) #TODO: need to catch whether post fail
-        #write update to twitter_updates
-        cur.execute(query_for_most_recent_updates)
-        row = cur.fetchone()
-        if row == None:
-            insert = 'INSERT INTO twitter_updates VALUES ('+str(month)+', ' +str(account[0]) +', '+ str(milestone_id)+')'
-            cur.execute(insert)
-            print insert
-            database.commit()
-        else:
-            updates = 'UPDATE twitter_updates SET milestone_id = '+str(milestone_id)+ ', month = '+str(month)+'  WHERE twitter_id = '+str(account[0])
-            cur.execute(updates)
-            print updates
-            database.commit()
-    else:
-        print 'no updates being made'
-            
+        #post milestone to twitter
+        print row
+        api = twitter.Api(consumer_key=account[2], consumer_secret=account[3], access_token_key=account[4], access_token_secret=account[5])
+        status = row[1]
+        print "original: "+status
+        milestone_id = row[0]
+        #replace %n% --> your baby; %pa%-->their
+        status = status.replace('%n%', 'your baby').replace('%pa%', 'their').replace('%s%', 'he/she').replace('%o%','him/her')
+        print "after replacement: "+status
+        api.PostUpdate(status)
+        cursor.close()
+        return milestone_id #return milestone id
+
+def updateMilestoneOnTwitterAccountTable(database, most_recent_milestone, account_id):
+    query = 'UPDATE twitteraccount SET most_recent_milestone = '+str(most_recent_milestone)+'  WHERE id = '+str(account_id)+";"
+    print query
+    cursor = database.cursor()
+    cursor.execute(query)
+    cursor.close()
+    database.commit()        
+
 def startPostTweets():
-    db = sqlite3.connect('test.db')
+    '''
+    go through the twitteraccount table and post one milestone for each account
+    '''
+    db = MySQLdb.connect(host="db.babystepsuw.org",user="babystepsdbadmin",
+                  passwd="vNRLtLf2Rhyy",db="babystepsdb")
     cur = db.cursor()
-    cur.execute("SELECT * FROM BabyStepsTwitterAccount")
+    cur.execute("SELECT * FROM twitteraccount;")
+    
     while True:
         row = cur.fetchone()
         if row == None:
             break
-        postTweet(db, row)
+        most_recent_milestone = postTweet(db, row)
+        if most_recent_milestone != None:
+            updateMilestoneOnTwitterAccountTable(db, most_recent_milestone, row[0])
+    
+    cur.close()
     db.close()
+    
 
 
 def main():
+    '''
     for i in range(10):
         startPostTweets()
         threading.Event().wait(30)
-    
+        '''
+    startPostTweets()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
